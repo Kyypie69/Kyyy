d63a8e4775b37ab95dc6f8f24c32dc886cbd5fd1
Kyypie69 <markyyroblox@gmail.com>
2025-09-19 22:59:31 +0800

    Update EditingKeyCodeFluent.lua

--- EditingKeyCodeFluent.lua
+++ EditingKeyCodeFluent.lua
@@ -1,12 +1,12 @@
 local key = loadstring(game:HttpGet("https://pastebin.com/raw/62f2jF5f",true))() -- put youre key in here
  
-local keylink = "Kupal.dof" -- put your key link here
+local keylink = "BINGOT.DOF" -- put your key link here
  
 local nameofthescript = "KYY Key" --put your script name here. leave blank too leave blank ig
  
 local whoisitmadeby = "MARKYY" -- put your name here
  
-local thenoteofthekey = "PUTANG INA NIYO DOF, CTX, ONYX!" --note you add here. leave blank to not have a note.
+local thenoteofthekey = "BAWAL SA PANGIT" --note you add here. leave blank to not have a note.
  
 -- Instances:
  
@@ -28,8 +28,8 @@ ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
  
 KeySystem.Name = "KeySystem"
 KeySystem.Parent = ScreenGui
-KeySystem.BackgroundColor3 = Color3.fromRGB(150, 175, 200)
-KeySystem.BorderColor3 = Color3.fromRGB(70, 10, 70)
+KeySystem.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+KeySystem.BorderColor3 = Color3.fromRGB(200, 0, 0)
 KeySystem.BorderSizePixel = 0
 KeySystem.Position = UDim2.new(0.379651636, 0, 0.32965821, 0)
 KeySystem.Size = UDim2.new(0.240300864, 0, 0.340683579, 0)
@@ -39,7 +39,7 @@ KeySystem.Draggable = true
  
 KeyTextbox.Name = "KeyTextbox"
 KeyTextbox.Parent = KeySystem
-KeyTextbox.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+KeyTextbox.BackgroundColor3 = Color3.fromRGB(0, 200, 0)
 KeyTextbox.BackgroundTransparency = 0.950
 KeyTextbox.BorderColor3 = Color3.fromRGB(0, 0, 0)
 KeyTextbox.BorderSizePixel = 0
@@ -47,7 +47,7 @@ KeyTextbox.Position = UDim2.new(0.0939044505, 0, 0.734627843, 0)
 KeyTextbox.Size = UDim2.new(0.813838542, 0, 0.161812291, 0)
 KeyTextbox.Font = Enum.Font.Gotham
 KeyTextbox.Text = "Enter Key"
-KeyTextbox.TextColor3 = Color3.fromRGB(255, 25, 255)
+KeyTextbox.TextColor3 = Color3.fromRGB(0, 200, 0)
 KeyTextbox.TextScaled = true
 KeyTextbox.TextSize = 14.000
 KeyTextbox.TextWrapped = true
@@ -60,7 +60,7 @@ UICorner_2.Parent = KeySystem
  
 title.Name = "title"
 title.Parent = KeySystem
-title.BackgroundColor3 = Color3.fromRGB(255, 0, 255)
+title.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
 title.BackgroundTransparency = 1.000
 title.BorderColor3 = Color3.fromRGB(0, 0, 0)
 title.BorderSizePixel = 0
@@ -68,13 +68,13 @@ title.Position = UDim2.new(0, 0, 0.0323624611, 0)
 title.Size = UDim2.new(1, 0, 0.197411001, 0)
 title.Font = Enum.Font.Gotham
 title.Text = "KEY SYSTEM"
-title.TextColor3 = Color3.fromRGB(255, 0, 255)
+title.TextColor3 = Color3.fromRGB(0, 0, 0)
 title.TextScaled = true
 title.TextSize = 14.000
 title.TextWrapped = true
  
 TextButton.Parent = KeySystem
-TextButton.BackgroundColor3 = Color3.fromRGB(25, 255, 25)
+TextButton.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
 TextButton.BackgroundTransparency = 0.950
 TextButton.BorderColor3 = Color3.fromRGB(0, 0, 0)
 TextButton.BorderSizePixel = 0
@@ -82,7 +82,7 @@ TextButton.Position = UDim2.new(0.0922570005, 0, 0.504854381, 0)
 TextButton.Size = UDim2.new(0.813838542, 0, 0.161812291, 0)
 TextButton.Font = Enum.Font.Gotham
 TextButton.Text = "Check Key"
-TextButton.TextColor3 = Color3.fromRGB(255, 255, 255)
+TextButton.TextColor3 = Color3.fromRGB(200, 0, 200)
 TextButton.TextScaled = true
 TextButton.TextSize = 14.000
 TextButton.TextWrapped = true
@@ -104,7 +104,7 @@ UICorner_3.Parent = TextButton
  
 scriptname.Name = "scriptname"
 scriptname.Parent = KeySystem
-scriptname.BackgroundColor3 = Color3.fromRGB(255, 0, 255)
+scriptname.BackgroundColor3 = Color3.fromRGB(200, 0, 0)
 scriptname.BackgroundTransparency = 1.000
 scriptname.BorderColor3 = Color3.fromRGB(0, 0, 0)
 scriptname.BorderSizePixel = 0
@@ -112,14 +112,14 @@ scriptname.Position = UDim2.new(0, 0, 0.262135923, 0)
 scriptname.Size = UDim2.new(1, 0, 0.0809061453, 0)
 scriptname.Font = Enum.Font.Gotham
 scriptname.Text = nameofthescript
-scriptname.TextColor3 = Color3.fromRGB(25, 255, 25)
+scriptname.TextColor3 = Color3.fromRGB(0, 200, 0)
 scriptname.TextScaled = true
 scriptname.TextSize = 14.000
 scriptname.TextWrapped = true
  
 madeby.Name = "madeby"
 madeby.Parent = KeySystem
-madeby.BackgroundColor3 = Color3.fromRGB(255, 0, 255)
+madeby.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
 madeby.BackgroundTransparency = 1.000
 madeby.BorderColor3 = Color3.fromRGB(0, 0, 0)
 madeby.BorderSizePixel = 0
@@ -127,7 +127,7 @@ madeby.Position = UDim2.new(0, 0, 0.368932039, 0)
 madeby.Size = UDim2.new(1, 0, 0.0809061453, 0)
 madeby.Font = Enum.Font.Gotham
 madeby.Text = "Made by: " .. whoisitmadeby
-madeby.TextColor3 = Color3.fromRGB(25, 255, 25)
+madeby.TextColor3 = Color3.fromRGB(0, 200, 0)
 madeby.TextScaled = true
 madeby.TextSize = 14.000
 madeby.TextWrapped = true
@@ -135,7 +135,7 @@ madeby.Visible = true
  
 note.Name = "note"
 note.Parent = KeySystem
-note.BackgroundColor3 = Color3.fromRGB(255, 0, 255)
+note.BackgroundColor3 = Color3.fromRGB(200, 0, 200)
 note.BackgroundTransparency = 1.000
 note.BorderColor3 = Color3.fromRGB(0, 0, 0)
 note.BorderSizePixel = 0
@@ -143,7 +143,7 @@ note.Position = UDim2.new(0, 0, 0.919093847, 0)
 note.Size = UDim2.new(1, 0, 0.0809061453, 0)
 note.Font = Enum.Font.Gotham
 note.Text = thenoteofthekey
-note.TextColor3 = Color3.fromRGB(25, 255, 25)
+note.TextColor3 = Color3.fromRGB(0, 0, 0)
 note.TextScaled = true
 note.TextSize = 14.000
 note.TextWrapped = true
